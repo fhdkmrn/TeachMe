cb6e50175e37dad5e6d1b3e7fe80efbb6ca7a023
Fahad Kamran <fhdkmrn@gmail.com>
2016-11-13 01:01:49 -0800

    Karma and email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161111195040) do
+ActiveRecord::Schema.define(version: 20161113084539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20161111195040) do
     t.datetime "oauth_expires_at"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.integer  "karma"
+    t.string   "email"
   end
 
 end
