2a563c2e39b6dd14220ead0dd19522f8a6bd616e
James Jiang <jmendre@berkeley.edu>
2016-11-17 16:25:34 -0800

    stats

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,8 +1,8 @@
 class CoursesController < ApplicationController
   before_action :set_course, only: [:show, :edit, :update, :destroy]
-
   # GET /courses
   # GET /courses.json
+
   def index
     @courses = Course.all
   end
--- classes/parser.rb
+++ classes/parser.rb
@@ -3,17 +3,19 @@ content = file.read
 courses = content.split("\n\n")
 for course in courses
 	key_content = course.split("\n")[0]
-	department = key_content.split(" ")[0]
-	course_number = key_content.split(" ")[1]
-	course_name = ""
-	for i in key_content.split(" ")[2...key_content.split(" ").count]
-		if (!!(i =~ /\A[-+]?[0-9]+\z/))
-			break
-		elsif i == nil
+	if !key_content.include?("[+]")
+		department = key_content.split(" ")[0]
+		course_number = key_content.split(" ")[1]
+		course_name = ""
+		for i in key_content.split(" ")[2...key_content.split(" ").count]
+			if (!!(i =~ /\A[-+]?[0-9]+\z/)) or i.includes?(".")
+				break
+			elsif i == nil
 
-		else
-			course_name = course_name + " " + i 
+			else
+				course_name = course_name + " " + i 
+			end
 		end
+		Course.create({:name=>course_name,:course_no=>course_number,:department=>department})
 	end
-	Course.create({:name=>course_name,:course_no=>course_number,:department=>department})
 end
