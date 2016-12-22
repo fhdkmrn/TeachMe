1e571f7abaa08cac69559d95040b1918e845aae6
Fahad Kamran <fhdkmrn@gmail.com>
2016-12-22 12:11:22 -0500

    Setup Dashboard in new controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,18 +9,4 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  def dashboard
-  	@user = User.find_by_id(session[:user_id])
-  	if @user == nil or @user.year == nil or @user.major == ""
-  		session[:user_id] = nil
-  		redirect_to root_path
-  	end
-  end
-
-  def search
-    respond_to do |format|
-      format.json {render :json => Course.text_search(params[:term])}
-    end
-  end
-
 end
