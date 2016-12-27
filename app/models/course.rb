a0c135727301c72a9d995a64a8f4d3e8ce6c2809
Tommy Huynh <TommysMAC@Tommys-MBP.home>
2016-12-27 14:22:10 -0800

    fixed autocomplete

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,7 +2,7 @@ class Course < ActiveRecord::Base
 
   def self.text_search(query)
     if query.present?
-      where("name ilike :q or department ilike :q or course_no ilike :q", q: "%#{query}%")
+      where("name ilike :q or department ilike :q or course_no ilike :q or full_title ilike :q", q: "%#{query}%")
     else
       Course.none
     end
