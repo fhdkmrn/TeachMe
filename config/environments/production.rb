5e97a9e083d14f6dfe21c39df126894a322bb7c4
Fahad Kamran <fhdkmrn@gmail.com>
2017-01-05 09:41:00 -0500

    Email fix maybe

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,19 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.serve_static_assets = true
 
+  config.action_mailer.default_url_options = { :host => 'cal-teachme.herokuapp.com' }  
+config.action_mailer.delivery_method = :smtp  
+config.action_mailer.perform_deliveries = true  
+config.action_mailer.raise_delivery_errors = false  
+config.action_mailer.default :charset => "utf-8"  
+config.action_mailer.smtp_settings = {  
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "cal-teachme.herokuapp.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
+}
+
 end
