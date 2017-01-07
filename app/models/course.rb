e94a69502ca9ca5721045c8429d2ee12a99e4564
Tommy Huynh <TommysMAC@Tommys-MBP.home>
2017-01-07 15:06:32 -0800

    autocomplete more accurate and better looking

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,7 +2,8 @@ class Course < ActiveRecord::Base
 
   def self.text_search(query)
     if query.present?
-      where("name ilike :q or department ilike :q or course_no ilike :q or full_title ilike :q", q: "%#{query}%")
+      where("name ilike :q or department ilike :q or course_no ilike :q or full_title ilike :q
+      or name @@ :p or department @@ :p or course_no @@ :p or full_title @@ :p", q: "%#{query}%", p: query).order('full_title DESC')
     else
       Course.none
     end
