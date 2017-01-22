eeabf02aa30131670d0a64fdb10ede23c404ce6b
Tommy Huynh <tommyhuynh@berkeley.edu>
2017-01-22 10:32:22 -0800

    mailer

--- app/mailers/request_mailer.rb
+++ app/mailers/request_mailer.rb
@@ -4,7 +4,8 @@ class RequestMailer < ApplicationMailer
 	def need_help_accepted(user, request)
 		@user = user
 		@request = request
-		url = "http://cal-teachme.herokuapp.com/request/confirm/" + @request.id.to_s +"/" + @user.id.to_s
+		"The #{animal} #{action} the #{second_animal}"
+		url = "http://cal-teachme.herokuapp.com/request/confirm/#{@request.id}/#{@user.id}"
     @confirmURL = ShortURL.shorten(url, :tinyurl)
 		receiver = User.find_by_id(@request.user).email
 		subj = "Someone is Ready to Tutor You!"
@@ -14,7 +15,7 @@ class RequestMailer < ApplicationMailer
 	def giving_help_accepted(user, request)
 		@user = user
 		@request = request
-		url = "http://cal-teachme.herokuapp.com/request/confirm/" + @request.id.to_s + "/" + @user.id.to_s
+		url = "http://cal-teachme.herokuapp.com/request/confirm/#{@request.id}/#{@user.id}"
     @confirmURL = ShortURL.shorten(url, :tinyurl)
 		receiver = User.find_by_id(@request.user).email
 		subj = "Someone would like to be Tutored by You!"
